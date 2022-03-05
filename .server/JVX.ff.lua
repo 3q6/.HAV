f8f08f64b1c9be609b6da3c8e4792cf98978368f
Haven <73487410+3q6@users.noreply.github.com>
2022-03-05 20:50:19 +0000

    Create JVX.ff.lua

---
+++ .server/JVX.ff.lua
@@ -0,0 +1,18 @@
+print("Working Files - JVX")
+
+--// Mit: @UIFrameworks / 10/10/2018
+
+local s = {}
+local admin = {}
+
+function admin:login(user, pass, token, host, hosturl)
+  print("Admin Host")
+  function s:interupt(host)
+    print("If Running")
+    if host == "1184" then
+      print("admin host")
+      print("can't login with source")
+    end
+end
+
+function s:run()
